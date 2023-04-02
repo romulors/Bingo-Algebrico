8134e0d94030224211cd4642a0026d110394e12f
Romulo Rocha Santos <romulo.ciencia@gmail.com>
2023-04-02 15:18:36 -0300

    Adiciono funcionalidade de abas

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-const p1 = document.getElementById('p1');
+const menuElements = document.querySelectorAll('.menu');
+const conteudoElements = document.querySelectorAll('.conteudo');
 
-// p1.innerHTML = math.
+// Inicia a configuração da página
+iniciaSeletorAbas();
+
+
+
+// Adiciona a funcionalidade de troca de abas
+function iniciaSeletorAbas() {
+
+	menuElements.forEach( (menu, index) => {
+		menu.addEventListener('click', () => {
+			
+			// Remove a classe .active de todas opcoes de menu e de conteudo
+			menuElements.forEach( opcaoDisponivel => opcaoDisponivel.classList.remove('active'));
+			conteudoElements.forEach( conteudoDisponivel => conteudoDisponivel.classList.remove('active'));
+
+			// Torna ativa a opcão do menu e seu respectivo conteudo
+			menuElements[index].classList.add('active');
+			conteudoElements[index].classList.add('active');
+			
+		})
+	})
+}
+
+console.log(menuElements);
+console.log(conteudoElements);
+
