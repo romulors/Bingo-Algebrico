a1c8fc400a0ae7100e26c42a048f354d0a3991a7
Romulo Rocha Santos <romulo.ciencia@gmail.com>
2023-04-02 18:28:23 -0300

    Adicionado modelos padrões de equações

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
-const menuElements = document.querySelectorAll('.menu');
+const menuElements = document.querySelectorAll('.menuItem');
 const conteudoElements = document.querySelectorAll('.conteudo');
+
+// Cria variaveis globais
+let modelos = [];
 
 // Inicia a configuração da página
 iniciaSeletorAbas();
+carregaModelosEquacoes();
 
+console.log(modelos);
+
+// Carrega arquivos de modelos padrões da pagina
+// modeloSomaFracoes = fetch('./modelos/fracoes_soma.json')
+// .then((response) => response.json())
+// .then((json) => console.log(json));
+
+
+// Carrega todos os modelos padrões da página
+async function carregaModelosEquacoes() {
+	try {
+		const diretorio = "modelos/";
+		//const hrefs = await verificaModelosEquacoes(diretorio);
+
+		hrefs = [];
+		hrefs.push("./modelos/fracoes_divisao.json");
+		hrefs.push("./modelos/fracoes_multiplicacao.json");
+		hrefs.push("./modelos/fracoes_soma.json");
+		hrefs.push("./modelos/fracoes_subtracao.json");
+
+		hrefs.forEach(ref => {
+			fetch(ref)
+			.then((response) => response.json())
+			.then((json) => modelos.push(json));
+		})
+
+		console.log(hrefs);
+	} catch (err) {
+		console.log(err);
+	}
+ }
+
+// Verifica quais modelos existem no diretório modelos
+async function verificaModelosEquacoes(diretorio) {
+	try {
+		const response = await fetch(diretorio);
+		const html = await response.text();
+		console.log("IMPRIMINDO HTML");
+		console.log(html);
+		const div = document.createElement('div');
+		div.innerHTML = html;
+		const anchorTags = div.querySelectorAll('a.icon-application-json');
+		const hrefs = [];
+		anchorTags.forEach(tag => {
+			hrefs.push(tag.href);
+		});
+		return hrefs;
+	} catch (err) {
+		console.log('Falha em verificar arquivos do diretório modelos', err);
+		return [];
+	}
+}
 
 
 // Adiciona a funcionalidade de troca de abas
@@ -23,7 +79,4 @@ function iniciaSeletorAbas() {
 		})
 	})
 }
-
-console.log(menuElements);
-console.log(conteudoElements);
 
